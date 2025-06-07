0c70558c11c46070ca4b97792327845cb65ebcba
Piyush Anand <piyush.anand@nagarro.com>
2025-06-08 00:35:59 +0530

    download-model route

--- server.js
+++ server.js
@@ -7,6 +7,16 @@ app.get('/', (req, res) => {
     return res.status(200).json({ "status": "UP" })
 })
 
+app.get('/download-model', (req, res) => {
+    const filePath = path.join(__dirname, 'DeepSeek-R1-Distill-Qwen-1.5B_multi-prefill-seq_q8_ekv1280.task');
+    res.download(filePath, (err) => {
+        if (err) {
+            console.error('Download error:', err);
+            res.status(500).send('Error downloading file.');
+        }
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server started with http://localhost:${PORT}`);
 })
